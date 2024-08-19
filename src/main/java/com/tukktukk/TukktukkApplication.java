f3460cab3ddaf684a73e4ba43ddf7f534b7110ed
Hoo-sung <gntjd135@gmail.com>
2024-08-19 17:17:30 +0900

    init: first commit

---
+++ src/main/java/com/tukktukk/TukktukkApplication.java
@@ -0,0 +1,13 @@
+package com.tukktukk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukktukkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukktukkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tukktukk/TukktukkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukktukk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukktukkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
